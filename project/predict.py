477e5ca60c1143ade6c91fbc808681cd8c365e01
Pranay-Pandey <pranaypandeyofficial@gmail.com>
2022-09-03 16:54:33 +0530

    firstcommit

---
+++ project/predict.py
@@ -0,0 +1,11 @@
+from transformers import PegasusForConditionalGeneration, PegasusTokenizer, Trainer, TrainingArguments
+import torch
+
+def predict(tx,model_name):
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    tokenizer = PegasusTokenizer.from_pretrained(model_name)
+    model = PegasusForConditionalGeneration.from_pretrained(model_name).to(device)
+    batch = tokenizer(tx, truncation=True, padding='longest', return_tensors="pt").to(device)
+    translated = model.generate(**batch)
+    tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
+    return tgt_text
