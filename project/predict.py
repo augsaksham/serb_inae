05a63a935922ab0648717c0542a2b50b97a52a88
augsaksham <sakshamdwivedi10@gmail.com>
2022-09-04 14:21:05 +0530

    final commit

--- project/predict.py
+++ project/predict.py
@@ -16,13 +16,15 @@ def predict(tx,model_name):
 
 
 def predict_test(df,model_name):
+    print("model name")
     dict_df={"Id":[],"Abstract":[],"RHS":[]}
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     tokenizer = PegasusTokenizer.from_pretrained(model_name)
     model = PegasusForConditionalGeneration.from_pretrained(model_name).to(device)
+    print("model loaded")
     for i in range(df.shape[0]):
         vl=df.iloc[i].values
-        dict_df["Id"].append(vl[0])
+        dict_df["FileName"].append(vl[0])
         dict_df["Abstract"].append(vl[1])
         batch = tokenizer(vl[1], truncation=True, padding='longest', return_tensors="pt").to(device)
         translated = model.generate(**batch)
